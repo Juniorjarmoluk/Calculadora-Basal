bd6af054dc73d63782298fa4fe139da830fe7971
Juniorjarmoluk <139251559+Juniorjarmoluk@users.noreply.github.com>
2023-07-14 13:16:17 -0400

    Add files via upload

---
+++ Calculadora/script.js
@@ -0,0 +1,18 @@
+let n1 = 50;
+
+let n2 = 70;
+
+let n3 = 100;
+
+function cualEsMayor(n1, n2, n3){
+if (n1 > n2 && n1 > n3){
+    console.log (n1)
+}
+if (n2 > n1 && n2 > n3){
+    console.log (n2)
+}
+if (n3 > n1 && n3 > n2){
+    console.log (n3)
+}
+}
+console.log(cualEsMayor(n1,n2,n3))
